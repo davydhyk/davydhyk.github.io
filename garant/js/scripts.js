9ded58fc173707a1747a2bae9c9ce503800c856f
davyd.hyk <davyd.hyk@arixess.com>
2018-11-04 01:26:07 +0200

    add new site page

---
+++ garant/js/scripts.js
@@ -0,0 +1,7 @@
+$(function () {
+  $('.partners-list').slick({
+    slidesToShow: 4,
+    prevArrow: '<div class="partners-arrow prev"><img src="img/prev-arrow.png" alt=""></div>',
+    nextArrow: '<div class="partners-arrow next"><img src="img/next-arrow.png" alt=""></div>'
+  });
+})
