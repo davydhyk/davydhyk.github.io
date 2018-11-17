cf7baf3447d6d8c19b3c069c4b4a8b89df5d6e17
davydhyk <davydhyk@gmail.com>
2018-11-17 15:53:38 +0200

    adaptive pages for garant site

--- garant/js/scripts.js
+++ garant/js/scripts.js
@@ -2,7 +2,27 @@ $(function () {
   $('.partners-list').slick({
     slidesToShow: 4,
     prevArrow: '<div class="partners-arrow prev"><img src="img/prev-arrow.png" alt=""></div>',
-    nextArrow: '<div class="partners-arrow next"><img src="img/next-arrow.png" alt=""></div>'
+    nextArrow: '<div class="partners-arrow next"><img src="img/next-arrow.png" alt=""></div>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
   });
 
   $('.reviews-slider-items').slick({
@@ -14,12 +34,52 @@ $(function () {
   $('.other-slider').slick({
     slidesToShow: 4,
     prevArrow: '<div class="other-arrow prev"><img src="img/prev-arrow.png" alt=""></div>',
-    nextArrow: '<div class="other-arrow next"><img src="img/next-arrow.png" alt=""></div>'
+    nextArrow: '<div class="other-arrow next"><img src="img/next-arrow.png" alt=""></div>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
   });
 
   $('.cmp-partners-list').slick({
     slidesToShow: 4,
     prevArrow: '<div class="cmp-partners-arrow prev"><img src="img/prev-arrow.png" alt=""></div>',
-    nextArrow: '<div class="cmp-partners-arrow next"><img src="img/next-arrow.png" alt=""></div>'
+    nextArrow: '<div class="cmp-partners-arrow next"><img src="img/next-arrow.png" alt=""></div>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
   });
 })
