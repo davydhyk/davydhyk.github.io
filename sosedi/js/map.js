7eb3d83ed3c263fa74b71e8c806a9e11218038e7
Davyd Huk <davyd.hyk@arixess.com>
2019-09-21 12:23:14 +0300

    add sosedi 4 pages

---
+++ sosedi/js/map.js
@@ -0,0 +1,254 @@
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 55.764160, lng: 37.600746},
+    zoom: 16,
+    disableDefaultUI: true,
+    styles: [
+      {
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#212121"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "color": "#212121"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.country",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#9e9e9e"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.locality",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#bdbdbd"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#181818"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#616161"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "color": "#1b1b1b"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#2c2c2c"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#8a8a8a"
+          }
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#373737"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#3c3c3c"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway.controlled_access",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#4e4e4e"
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#616161"
+          }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#000000"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#3d3d3d"
+          }
+        ]
+      }
+    ]
+  });
+
+  Popup = createPopupClass();
+  popup = new Popup(
+      new google.maps.LatLng(55.764160, 37.600746),
+      document.getElementById('map-marker'));
+  popup.setMap(map);
+}
+
+function createPopupClass() {
+  /**
+   * A customized popup on the map.
+   * @param {!google.maps.LatLng} position
+   * @param {!Element} content The bubble div.
+   * @constructor
+   * @extends {google.maps.OverlayView}
+   */
+  function Popup(position, content) {
+    this.position = position;
+
+    content.classList.add('popup-bubble');
+
+    // This zero-height div is positioned at the bottom of the bubble.
+    var bubbleAnchor = document.createElement('div');
+    bubbleAnchor.classList.add('popup-bubble-anchor');
+    bubbleAnchor.appendChild(content);
+
+    // This zero-height div is positioned at the bottom of the tip.
+    this.containerDiv = document.createElement('div');
+    this.containerDiv.classList.add('popup-container');
+    this.containerDiv.appendChild(bubbleAnchor);
+
+    // Optionally stop clicks, etc., from bubbling up to the map.
+    google.maps.OverlayView.preventMapHitsAndGesturesFrom(this.containerDiv);
+  }
+  // ES5 magic to extend google.maps.OverlayView.
+  Popup.prototype = Object.create(google.maps.OverlayView.prototype);
+
+  /** Called when the popup is added to the map. */
+  Popup.prototype.onAdd = function() {
+    this.getPanes().floatPane.appendChild(this.containerDiv);
+  };
+
+  /** Called when the popup is removed from the map. */
+  Popup.prototype.onRemove = function() {
+    if (this.containerDiv.parentElement) {
+      this.containerDiv.parentElement.removeChild(this.containerDiv);
+    }
+  };
+
+  /** Called each frame when the popup needs to draw itself. */
+  Popup.prototype.draw = function() {
+    var divPosition = this.getProjection().fromLatLngToDivPixel(this.position);
+
+    // Hide the popup when it is far out of view.
+    var display =
+        Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000 ?
+        'block' :
+        'none';
+
+    if (display === 'block') {
+      this.containerDiv.style.left = divPosition.x + 'px';
+      this.containerDiv.style.top = divPosition.y + 'px';
+    }
+    if (this.containerDiv.style.display !== display) {
+      this.containerDiv.style.display = display;
+    }
+  };
+
+  return Popup;
+}
---
+++ sosedi/js/scripts.js
@@ -0,0 +1,63 @@
+(function () {
+  var homeBgs = [];
+  $('.home-names').on('init', function(event, slick){
+    var slide = slick.currentSlide + 1;
+    $('.home-counter span:first-child').text('0' + slide);
+    $('.home-counter span:last-child').text('0' + slick.$slides.length);
+    $('.home-link a').attr('href', $(slick.$slides[slide]).data('link'));
+    $('.home-bgs :nth-child(' + slide + ')').addClass('active');
+    $('.home-text :nth-child(' + slide + ')').addClass('active');
+    for(var i=1;i<=slick.$slides.length;i++)
+      homeBgs.push($('.home-bgs :nth-child(' + i + ')').attr('style'))
+    setTimeout(() => {$('.home-bgs').addClass('trs');}, 0);
+  }).slick({
+    variableWidth: true,
+    infinite: false,
+    prevArrow: $('.home-ctrl :nth-child(1)'),
+    nextArrow: $('.home-ctrl :nth-child(2)'),
+    focusOnSelect: true,
+    speed: 1000
+  }).on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+    if (currentSlide == nextSlide) return;
+    currentSlide++; nextSlide++;
+    var curSlide = $('.home-names-item:nth-child(' + currentSlide + ')');
+    var nxtSlide = $('.home-names-item:nth-child(' + nextSlide + ')');
+    var curBg = $('.home-bgs :nth-child(' + currentSlide + ')');
+    var nxtBg = $('.home-bgs :nth-child(' + nextSlide + ')');
+    $('.home-counter span:first-child').text('0' + nextSlide);
+    $('.home-link a').attr('href', nxtSlide.data('link'));
+    var trsDuration = 400,
+        dir = nextSlide > currentSlide ? '-' : '',
+        s1 = homeBgs[currentSlide-1],
+        s2 = homeBgs[nextSlide-1];
+
+    $('.home-text :nth-child(' + currentSlide + ')').css('transition', 'none').fadeOut(300);
+    setTimeout(() => {$('.home-text :nth-child(' + currentSlide + ')').removeClass('active').removeAttr('style').fadeIn(100);}, 400);
+    setTimeout(() => {$('.home-text :nth-child(' + nextSlide + ')').addClass('active');}, 700);
+
+    $('.home-bgs .active').addClass('change');
+    nxtBg.css('z-index', '-2')
+    curBg.css('z-index', '-2')
+    setTimeout(() => {
+      nxtBg.css('transform', 'scale(0.5) translateX(0)').addClass('active');
+      curBg.css('transform', 'scale(0.5) translateX(' + dir + '129.2%)').removeClass('active');
+      setTimeout(() => {
+        curBg.removeClass('change');
+        nxtBg.attr('style', s2);
+        setTimeout(() => {
+          curBg.attr('style', s1);
+        }, trsDuration);
+      }, trsDuration);
+    }, trsDuration);
+  });
+
+  $('.menu > img').click(function () {
+    $(this).parent().toggleClass('active');
+  });
+
+  $('section.form form .left .input input').on('input', function () {
+    if ($(this).val().length) $(this).parent().parent().addClass('fill');
+    else $(this).parent().parent().removeClass('fill');
+  });
+
+})(jQuery);
